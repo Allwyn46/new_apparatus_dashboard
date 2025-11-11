6053380578fd06b9c0ac4c35778b1e93e34a29b3
Allwyn46 <allwyn153@gmail.com>
2025-11-11 18:26:33 +0530

    initial commit

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -63,52 +63,9 @@
                             <li><a href="{{ url('/category/view') }}" data-key="t-inbox">Categories</a></li>
                             <li><a href="{{ url('/subcategory/view') }}" data-key="t-inbox">Sub-Categories</a></li>
 
-                    {{-- <li>
-                        <a href="{{ url('/sale/index') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="lucide lucide-badge-dollar-sign-icon lucide-badge-dollar-sign">
-                                <path
-                                    d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
-                                <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8" />
-                                <path d="M12 18V6" />
-                            </svg>
-                            <span data-key="t-dashboard">Sales</span>
-admin@colors2cart.in                        </a>
-                    </li> --}}
+                    
 
-                    {{-- <li>
-                        <a href="{{ url('/order/view') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="lucide lucide-shopping-bag-icon lucide-shopping-bag">
-                                <path d="M16 10a4 4 0 0 1-8 0" />
-                                <path d="M3.103 6.034h17.794" />
-                                <path
-                                    d="M3.4 5.467a2 2 0 0 0-.4 1.2V20a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6.667a2 2 0 0 0-.4-1.2l-2-2.667A2 2 0 0 0 17 2H7a2 2 0 0 0-1.6.8z" />
-                            </svg>
-                            <span data-key="t-dashboard">Orders</span>
-                        </a>
-                    </li> --}}
-
-                    <li>
-                        <a href="javascript: void(0);" class="has-arrow">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="lucide lucide-bookmark-check-icon lucide-bookmark-check">
-                                <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2Z" />
-                                <path d="m9 10 2 2 4-4" />
-                            </svg>
-                            <span data-key="t-email">Bookings</span>
-                        </a>
-                        <ul class="sub-menu" aria-expanded="false">
-                            <li><a href="{{ url('/painter-booking/view') }}" data-key="t-inbox">Painter Booking</a></li>
-                            <li><a href="{{ url('/consulting/view') }}" data-key="t-inbox">Color Consultation
-                                    Booking</a>
-                            </li>
-                        </ul>
-                    </li>
+                    
 
                     <li>
                         <a href="javascript: void(0);" class="has-arrow">
